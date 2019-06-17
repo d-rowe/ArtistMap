1e6d0c6ecdf7a1938d28e873f1b3c40a318ffb70
Daniel Rowe <danielrowemusic@gmail.com>
2019-06-17 16:52:06 -0700

    moved to hooks, set viewport to North America

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,30 +1,26 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import ReactMapGL from "react-map-gl";
 import TOKEN from "../token";
 import "mapbox-gl/dist/mapbox-gl.css";
 
-class Map extends Component {
-  state = {
-    viewport: {
-      width: "100%",
-      height: "100%",
-      latitude: 47.608013,
-      longitude: -122.335167,
-      zoom: 9
-    }
-  };
+const Map = () => {
+  const [viewport, setViewport] = useState({
+    width: "100%",
+    height: "100%",
+    latitude: 42.877742,
+    longitude: -97.380979,
+    zoom: 3
+  });
 
-  render() {
-    return (
-      <ReactMapGL
-        mapStyle="mapbox://styles/mapbox/light-v9"
-        mapboxApiAccessToken={TOKEN}
-        {...this.state.viewport}
-        onViewportChange={viewport => this.setState({ viewport })}
-        onResize={size => console.log(size)}
-      />
-    );
-  }
-}
+  return (
+    <ReactMapGL
+      mapStyle="mapbox://styles/mapbox/light-v9"
+      mapboxApiAccessToken={TOKEN}
+      {...viewport}
+      onViewportChange={viewport => setViewport(viewport)}
+      onResize={size => console.log(size)}
+    />
+  );
+};
 
 export default Map;
