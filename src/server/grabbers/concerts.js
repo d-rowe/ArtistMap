c265922d66ad801152793ad6bc4dc71a6a17faba
Daniel Rowe <danielrowemusic@gmail.com>
2019-06-20 15:56:08 -0700

    moved grabbers into grabbers folder

--- src/server/grabbers/concerts.js
+++ src/server/grabbers/concerts.js
@@ -1,4 +1,4 @@
-require("dotenv").config({ path: __dirname + "/.env" });
+require("dotenv").config({ path: __dirname + "/../.env" });
 const fetch = require("node-fetch");
 const KEY = process.env.SONGKICK_KEY;
 
--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
-const getArtist = require("./artist");
-const getConcerts = require("./concerts");
+const getArtist = require("./grabbers/artist");
+const getConcerts = require("./grabbers/concerts");
 
 app.get("/api/artist", (req, res) => {
   getArtist(res, req.query.artistName);
